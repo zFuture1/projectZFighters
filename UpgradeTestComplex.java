d4dded31e122214b78a57ad65a2c0e0d5e422f2c
zFuture1 <markzambuto@gmail.com>
2016-03-29 09:34:54 -0500

    Added files via upload

---
+++ UpgradeTestComplex.java
@@ -0,0 +1,46 @@
+package Upgrader;
+
+public class UpgradeTestComplex {
+    
+    private char result = 0;
+    
+    public UpgradeTestComplex(int odds) {
+        createUpgradePattern(odds);
+    }
+
+    /** Create a pattern to draw the result from
+     * @param odds, the successful upgrade %.
+     */
+    private void createUpgradePattern(int odds) {
+        String pattern = "";
+        int patternLength = 0;
+        int patternComplete = 100;
+        
+        int builder = 1;
+        while(patternLength < patternComplete) {
+            if(builder % odds == 0) {
+                pattern += "0";
+                patternLength++;
+            }
+            if(builder % (100 - odds) == 0) {
+                pattern += "1";
+                patternLength++;
+            }
+            builder++;
+        }
+        chooseUpgradeResult(pattern);
+    } 
+    
+    /** Determine the upgrade result
+     * @param pattern, the combination of pass / fail bits.
+     */
+    private void chooseUpgradeResult(String pattern) {
+        int choice = (int)Math.round(Math.random() * 99);
+        result = pattern.charAt(choice);
+    }
+    
+    /** Get the upgrade result */
+    public char getResult() {
+        return result;
+    }
+}
